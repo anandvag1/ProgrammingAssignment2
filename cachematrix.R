13773db1bd0db5724e1e9ad9df2658737c18e02d
anandvag1 <anandvag1@gmail.com>
2019-10-16 22:52:13 +0530

    Final version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A function to create a special vector of matrix function and another function
+## to get the inverse from the special vector and if no inverse calcialte the inverse
 
-## Write a short comment describing this function
+## A function to create a special vector of matrix function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  iMatrix <- NULL
+  
+  setM <- function(y){
+    x <<- y
+    iMatrix <<- NULL
+  }
+  
+  getM <- function() x
+  
+  getIMatrix <- function() iMatrix
+  
+  setIMatrix <- function(im) iMatrix <<- im
+  
+  list(setM = setM, getM = getM,
+       setIMatrix = setIMatrix,
+       getIMatrix = getIMatrix)
 
 }
 
 
-## Write a short comment describing this function
+## Function to get the inverse from the special vector and if no inverse calcialte the inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  iMatrix <- x$getIMatrix()
+  
+  if (!is.null(iMatrix)){
+    message("getting cached data")
+    return(iMatrix)
+  }
+  
+  mat <- x$getM()
+  
+  iMatrix <- solve(mat)
+  x$setIMatrix(iMatrix)
+  iMatrix
+  
 }
